fbfa89e3cfb9e1a44e7efe1ee740b0d8b21a79cb
ashokq <pemaramashok87277@gmail.com>
2023-07-24 10:15:47 +0530

    JS Task 1

--- Pattern/Diamond Pattern.js
+++ Pattern/Diamond Pattern.js
@@ -1,2 +1,26 @@
-console.log("Diamond");
-console.log("ashok");
+function diamond_pattern(num){
+    // var row ="";
+    let space=Math.floor(num/2);
+    let star =1;
+
+    for(let i=0; i<num; i++){ //loop for iterate rows
+    let row="";
+         for(let j=0;j<space;j++){//loop for print spaces in row
+            row+=" ";
+         }
+         for(let k=0;k<star;k++){//loop for print starts in row
+            row+="*";
+         }
+         console.log(row);
+         if(i<Math.floor(num/2)){
+            space--;
+            star+=2;
+         }else{
+            space++;
+            star-=2;
+         }
+    }
+}
+diamond_pattern(5);
+
+
